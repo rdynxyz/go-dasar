299b8c01d9403ad59e87ec6ccee95840967457fa
Radyan Bintang <113041507+rdynxyz@users.noreply.github.com>
2023-09-16 11:28:43 +0700

    go

---
+++ string.go
@@ -0,0 +1,9 @@
+package main 
+
+import "fmt"
+
+func main(){
+	fmt.Println(len("Radyan")) //jumlah karakter di string
+	fmt.Println("Radyan"[0]) //karakter ke 1 (0) di string
+	fmt.Println("Radyan"[1]) //karakter ke 2 (1) di string
+}	
